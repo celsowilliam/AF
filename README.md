44c73e66167910592d9e8507e5e8abe51b6d4b6d
Celso William <celsowilliam299@gmail.com>
2026-06-01 17:44:12 -0300

    Primerios commits, contato, galeria e index

---
+++ README.md
@@ -0,0 +1,15 @@
+# CineVerse
+
+Site sobre filmes e séries.
+
+## Integrantes
+- Celso William Leite Ferreira - RA: 251053
+
+## Tecnologias
+- HTML5
+- CSS3
+- Bootstrap
+- JavaScript
+
+## Status
+Em desenvolvimento...
---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*
+AF - Avaliação Final - Linguagens de Programação
+Tema do Projeto: CineVerse - Filmes e Séries
+Aluno 1: Celso William Leite Ferreira - RA: 251053
+Professor: Abimael de Oliveira
+Data de entrega: 07/06
+*/
