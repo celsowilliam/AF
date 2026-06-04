9835857a92f3e362eb372f505b00bfb89efefba1
Celso William <celsowilliam299@gmail.com>
2026-06-04 02:27:57 -0300

    Inserindo galeria e css

--- js/script.js
+++ js/script.js
@@ -5,3 +5,24 @@ Aluno 1: Celso William Leite Ferreira - RA: 251053
 Professor: Abimael de Oliveira
 Data de entrega: 07/06
 */
+
+// Voltar ao topo
+const btnTopo = document.querySelector('#btnTopo');
+
+if (btnTopo) {
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 300) {
+            btnTopo.classList.add('mostrar');
+        } else {
+            btnTopo.classList.remove('mostrar');
+        }
+    });
+
+    btnTopo.addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+}
+window.scrollTo()
